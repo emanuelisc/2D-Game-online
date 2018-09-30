e03fc5679054f340ec0b76cabd91afcca05163c5
Unknown <emanuelisc@gmail.com>
2018-09-30 23:01:56 +0300

    New simpler game

---
+++ new_game/src/MainGame/MainClass.java
@@ -0,0 +1,22 @@
+package MainGame;
+
+import javax.swing.*;
+
+public class MainClass extends JFrame {
+
+    public MainClass() {
+
+        add(new Map());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize((180 * 5) + 6, (110 * 5) + 28);
+        setLocationRelativeTo(null);
+        setTitle("RESIST");
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainClass();
+    }
+}
